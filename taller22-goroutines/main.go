b5a29f9667136880cace7778b964ed3652c48329
zarodriguezes <zarodriguezes@uide.edu.ec>
2026-07-27 14:45:52 +0000

    taller22: primera iteracion inicial

---
+++ taller22-goroutines/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "fmt"
+)
+type Order struct {
+  ID     int
+  Status string
+}
+func main() {
+  fmt.Print("Todas las operaciones completadas. Finalizando\n")
+}
+
+func main() {
+  orders := generateOrders(20)
+  fmt.Print("Todas las operaciones completadas. Finalizando\n")
+}
+func generateOrders(count int) []*Order {
+  orders := make([]*Order, count)
+  for i := 0; i < count; i++ {
+    orders[i] = &Order{
+      ID: i + 1, Status: "pending",
+    }
+  }
+  return orders
+}
