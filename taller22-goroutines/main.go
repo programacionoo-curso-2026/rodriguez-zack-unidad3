ac2414ee665d67340afa78d831b8baa31f996458
zarodriguezes <zarodriguezes@uide.edu.ec>
2026-07-28 15:32:07 +0000

    Taller22: cuarta interacion

--- taller22-goroutines/main.go
+++ taller22-goroutines/main.go
@@ -16,8 +16,22 @@ func main() {
 
 	processOrders(orders)
 
+	updateOrderStatuses(orders)
+
+	reportOrderStatus(orders)
+
 	fmt.Printf("Numero de Orderenes %d\n", len(orders))
 	fmt.Print("Todas las operaciones completadas. Finalizando\n")
+}
+
+func updateOrderStatuses(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		status := []string{"Procesando", "Despachando", "Entregando"}[rand.Intn(3)]
+		order.Status = status
+		fmt.Printf("Actualizando la orden %d con estado %s\n", order.ID, status)
+
+	}
 }
 
 func generateOrders(count int) []*Order {
@@ -35,5 +49,17 @@ func processOrders(orders []*Order) {
 		delay := rand.Intn(500)
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 		fmt.Printf("Procesando orden %d en %dms\n", order.ID, delay)
+	}
+}
+
+func reportOrderStatus(orders []*Order) {
+	for i := 0; i < 3; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("\n--- Reporte Estado de las Ordenes ---\n")
+		for _, order := range orders {
+			fmt.Printf("Orden %d %s\n",
+				order.ID, order.Status)
+		}
+		fmt.Printf("---------------------------------------\n")
 	}
 }
