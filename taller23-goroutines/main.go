0831efcdc4233b418dad9c6e9cd0026170c0feba
zarodriguezes <zarodriguezes@uide.edu.ec>
2026-08-07 13:13:57 +0000

    Taller 23: Tecera interaccion con main.go

--- taller23-goroutines/main.go
+++ taller23-goroutines/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	/*"fmt"*/
-	/*"math/rand"*/
+	"fmt"
+	"math/rand"
 	"sync"
-	/*"time"*/)
+	"time"
+)
 
 type Order struct {
 	ID     int
@@ -16,3 +17,48 @@ var (
 	totalUpdates int
 	updateMutex  sync.Mutex
 )
+
+func main() {
+	var wg sync.WaitGroup
+	orders := generateOrders(20)
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatus(order)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Print("Todas las operaciones completadas. Exiting\n")
+	fmt.Printf("Total Actualizaciones %d\n", totalUpdates)
+
+}
+
+func updateOrderStatus(order *Order) {
+	order.mu.Lock()
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
+	status := []string{"Procesando", "Despachando", "Entregando"}[rand.Intn(3)]
+	order.Status = status
+	fmt.Printf("Actualizando orden %d con estado: %s\n", order.ID, status)
+	order.mu.Unlock()
+
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
+
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
+
+}
+
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+	for i := 0; i < count; i++ {
+		orders[i] = &Order{
+			ID: i + 1, Status: "pending",
+		}
+	}
+	return orders
+}
